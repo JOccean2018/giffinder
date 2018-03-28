0f25c9956cd73e96f2e67141350867b4043c3065
Jean Occean <joccean2018@bklawtech.com>
2018-03-28 22:33:32 +0000

    new

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
-// *************************** YOUR CODE BELOW *******************************
-//******************TEST EARLY AND OFTEN USING console.log() ******************
-//****************** SERIOUSLY TEST USING console.log()!!! ******************
+function giphyURLWithSearchTerm(searchTerm) {
+    
+    return "https://api.giphy.com/v1/stickers/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC";
+     
+    // write a function that will return a url for the giphy API with
+    // the searchTerm provided in the parameters
+   }
 
-$(document).ready(function(){
-  
-  
-  
-  
+
+function appendImageToBody(srcURL) {
+    // write a function that will append an <img> to the body with the
+    // URL provided in the parameters
+    $('body').append('<img src=' + srcURL + '>');
+    
+}
+
+function callGiphyAPIWithSearchTerm(searchTerm) {
+    $.ajax({
+      url: giphyUrlWithSearchTerm(searchTerm) ,
+      method: "GET",
+      success: function(response) {
+           var url = response.data[0].images.original.url;
+           appendImageToBody(url);
+      },
+    }); 
+}
+
+//Add a click handler below to call the function when the button is clicked
+
+   
+  $("button").click(function() {
+var searchTerm = $("input").val;
+ callGiphyAPIWithSearchTerm(searchTerm);
 });
