04f3e5fc969064f3009d52934b57759e334d69d1
Jean Occean <joccean2018@bklawtech.com>
2018-04-11 22:22:20 +0000

    working

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
 function giphyURLWithSearchTerm(searchTerm) {
-    
-    return "https://api.giphy.com/v1/stickers/search?q="+searchTerm+"&api_key=dc6zaTOxFJmzC";
-     
     // write a function that will return a url for the giphy API with
     // the searchTerm provided in the parameters
-   }
-
+    
+}
 
 function appendImageToBody(srcURL) {
     // write a function that will append an <img> to the body with the
     // URL provided in the parameters
-    $('body').append('<img src=' + srcURL + '>');
-    
+    console.log(srcURL)
+    $('#newGif').append('<img src=' + srcURL + '>');
 }
 
 function callGiphyAPIWithSearchTerm(searchTerm) {
     $.ajax({
-      url: giphyUrlWithSearchTerm(searchTerm) ,
+      url: "https://api.giphy.com/v1/stickers/search?q=" + searchTerm + "&api_key=dc6zaTOxFJmzC",
       method: "GET",
       success: function(response) {
-           var url = response.data[0].images.original.url;
+           var url = response.data[2].images.original.url;
            appendImageToBody(url);
       },
     }); 
 }
 
-//Add a click handler below to call the function when the button is clicked
 
-   
-  $("button").click(function() {
-var searchTerm = $("input").val;
- callGiphyAPIWithSearchTerm(searchTerm);
+//Add a click handler below to call the function when the button is clicked
+$("button").click(function (searchTerm) {
+    var searchTerm = $("input").val();
+    callGiphyAPIWithSearchTerm(searchTerm);
+    $('h1').empty();
 });
+
+    
